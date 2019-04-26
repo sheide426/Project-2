2e4e70c05e8a9b3ccf69e7a49f9a7ee0162efeb1
Sarah Heidegger <sheide@artic.edu>
2019-04-25 20:58:17 -0500

    added
    teleportation
    vrtk
    some objects

---
+++ Final_Project_sheide/Assets/scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public Object theScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(theScene.name, LoadSceneMode.Single);
+    }
+}
+
