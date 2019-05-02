d5b74c86f7d7571803b73d66704ee7910869c76a
Sarah Heidegger <sheide@artic.edu>
2019-05-02 01:09:34 -0500

    done with hell
    just gotta make sure everything works

---
+++ Final_Project_sheide/Assets/scripts/BasicAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BasicAI : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform myTransform;
+
+
+
+    void Update()
+    {
+
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+
+    }
+}
+
